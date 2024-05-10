2d342c6ade65c3f627077936f642f660022544b3
dataslid <azeezolabode010@gmail.com>
2024-05-10 05:09:13 -0400

    ws

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 // 
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 80 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
