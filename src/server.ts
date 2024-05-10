936640a1d73f51a62e5ea29c699fb2d5a223dbae
dataslid <azeezolabode010@gmail.com>
2024-05-10 13:31:55 +0100

    metrics

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 // 
 import { WebSocketServer } from 'ws';
+// import si from "systeminformation";
+import * as si from "systeminformation";
 
 const wss = new WebSocketServer({ port: 80 });
 
@@ -9,8 +11,12 @@ wss.on('connection', function connection(ws) {
     ws.send('something');
   });
 
-  setInterval(() => {
-    ws.send('something 1');
-  }, 5000)
-  
+  // setInterval(() => {
+  //   ws.send('something 1');
+  // }, 5000)
+
+  setInterval(async () => {
+    const cpuTemp = JSON.stringify(await si.currentLoad());
+    ws.send(cpuTemp);
+  }, 1000);
 });
